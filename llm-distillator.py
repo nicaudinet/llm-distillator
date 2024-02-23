6b1fd207ab0943c128c6e301dafa21873bf13fd3
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-02-23 21:46:09 +0100

    Simple argparse

--- llm-distillator.py
+++ llm-distillator.py
@@ -1,10 +1,20 @@
+import argparse
+from enum import Enum
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-if __name__ == "__main__":
 
+class RunMode(Enum):
+    ALVIS = "alvis"
+    OLLAMA = "ollama"
+
+
+def run_alvis():
     model_name = "mistralai/Mistral-7B-Instruct-v0.2"
     device = "cuda"
-    model_filepath = "/mimer/NOBACKUP/groups/ci-nlp-alvis/models/Mistral-7B-Instruct-v0.2"
+    model_filepath = (
+        "/mimer/NOBACKUP/groups/ci-nlp-alvis/models/Mistral-7B-Instruct-v0.2"
+    )
 
     model = AutoModelForCausalLM.from_pretrained(model_filepath)
     tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
@@ -18,3 +28,22 @@ if __name__ == "__main__":
     decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
     print(decoded[0])
+
+
+def run_ollama():
+    print("Running model using Ollama")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Distill text with an LLM")
+    parser.add_argument(
+        "runmode",
+        choices=[m.value for m in RunMode],
+        help="How to run the model",
+    )
+    args = parser.parse_args()
+
+    if args.runmode == RunMode.ALVIS.value:
+        run_alvis()
+    else:
+        run_ollama()
