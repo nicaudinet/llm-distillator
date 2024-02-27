15b058295dc4c37a5f3afe6cbf8d593e3c939a42
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-02-27 14:09:49 +0100

    Inference with pipeline on Alvis
    Second attempt at pipelines
    
    pipeline finally works

--- runner.py
+++ runner.py
@@ -1,7 +1,9 @@
 from enum import Enum
+from pathlib import Path
 
 import ollama
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import (AutoModelForCausalLM, AutoTokenizer, Conversation,
+                          ConversationalPipeline)
 
 
 class RunMode(Enum):
@@ -23,27 +25,9 @@ class LLMRunner:
         """
         print(".")
         if self.runmode == RunMode.ALVIS:
-            return self._run_alvis(prompt)
+            return self._run_alvis_pipeline(prompt)
         else:
             return self._run_ollama(prompt)
-
-    def _run_alvis(self, prompt: str) -> str:
-        """
-        Run Mistral 7B on my Alvis project with a prompt
-        """
-        model_name = "mistralai/Mistral-7B-Instruct-v0.2"
-        device = "cuda"
-        model_filepath = (
-            "/mimer/NOBACKUP/groups/ci-nlp-alvis/models/Mistral-7B-Instruct-v0.2"
-        )
-        model = AutoModelForCausalLM.from_pretrained(model_filepath)
-        tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
-        encodends = tokenizer(prompt, return_tensors="pt")
-        model.to(device)
-        model_inputs = encodends.to(model.device)
-        generated_ids = model.generate(**model_inputs, max_new_tokens=200)
-        decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-        return decoded[0]
 
     def _run_ollama(self, prompt: str) -> str:
         """
@@ -54,3 +38,35 @@ class LLMRunner:
             messages=[{"role": "user", "content": prompt}],
         )
         return response["message"]["content"]
+
+    def _run_alvis(self, prompt: str) -> str:
+        """
+        Run Mistral 7B on my Alvis project with a prompt
+        """
+        model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+        device = "cuda"
+        model_filepath = ()
+        model = AutoModelForCausalLM.from_pretrained(model_filepath)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+        encodends = tokenizer(prompt, return_tensors="pt")
+        model.to(device)
+        model_inputs = encodends.to(model.device)
+        generated_ids = model.generate(**model_inputs, max_new_tokens=200)
+        decoded = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        return decoded[0]
+
+    def _run_alvis_pipeline(self, prompt: str) -> str:
+        in_dir = Path("/mimer/NOBACKUP/groups/ci-nlp-alvis/")
+        model_path = in_dir / Path("models/Mistral-7B-Instruct-v0.2")
+        tokenizer_path = in_dir / Path("tokenizers/Mistral-7B-Instruct-v0.2")
+        model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        distillator = ConversationalPipeline(
+            model=model,
+            tokenizer=tokenizer,
+            batch_size=2,
+            framework="pt",
+        )
+        conversation = Conversation(prompt)
+        conversation = distillator(conversation)
+        return conversation.messages[-1]["content"]
