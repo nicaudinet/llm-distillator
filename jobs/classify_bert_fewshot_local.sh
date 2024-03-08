a4c75f981ba7b1edd2ce17a57eafc1d2e36fe054
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-03-08 14:06:07 +0100

    Jobscript to run few-shot BERT classification locally

---
+++ jobs/classify_bert_fewshot_local.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+python classify_bert.py \
+    --original_reviews="data/amazon_reviews/original_reviews.txt" \
+    --distilled_reviews="data/amazon_reviews/few-shot/distilled_reviews.txt" \
+    --out_file="data/amazon_reviews/few-shot/results_local.txt" \
+    --num_reviews=20 \
+    --batch_size=4 \
+    --test_size=0.2 \
+    --num_bootstrap=20 \
+    --confidence_level=0.95 \
