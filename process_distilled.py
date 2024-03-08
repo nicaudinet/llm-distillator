ce35b0500d467416808fb989cb188f108902860c
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-03-08 11:47:57 +0100

    Process few-shot distilled reviews

--- process_distilled.py
+++ process_distilled.py
@@ -1,15 +1,17 @@
 import os
 from pathlib import Path
 
-result_dir = Path("data/amazon_reviews/distilled/results")
-output_path = Path("data/amazon_reviews/distilled.txt")
+result_dir = Path("data/amazon_reviews/distilled/few-shot/")
+output_path = Path("data/amazon_reviews/distilled/few-shot.txt")
 
-files = os.listdir(result_dir)
-files = [int(file.split(".")[0]) for file in files]
-files = [str(file).zfill(3) + ".txt" for file in sorted(files)]
+# files = os.listdir(result_dir)
+# files = [int(file.split(".")[0]) for file in files]
+# files = [str(file).zfill(5) + ".txt" for file in sorted(files)]
 
+files = sorted(os.listdir(result_dir))
 with open(output_path, "a") as out_file:
     for file in files:
+        print(file)
         with open(result_dir / Path(file), "r") as in_file:
             lines = in_file.readlines()
         lines = [line.strip().lower() for line in lines if line.strip()]
