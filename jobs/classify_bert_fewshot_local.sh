483dfda29c30cd9c2f934033b3d4c3bf31123635
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-03-11 16:59:32 +0100

    Run full experiments on 5000 reviews

--- jobs/classify_bert_fewshot_local.sh
+++ jobs/classify_bert_fewshot_local.sh
@@ -5,8 +5,8 @@ python classify.py \
     --original_reviews="data/amazon_reviews/original_reviews.txt" \
     --distilled_reviews="data/amazon_reviews/few-shot/distilled_reviews.txt" \
     --out_file="data/amazon_reviews/few-shot/results_bert_local.txt" \
-    --num_reviews=20 \
+    --num_reviews=5000 \
     --batch_size=4 \
     --test_size=0.2 \
-    --num_bootstrap=20 \
+    --num_bootstrap=500 \
     --confidence_level=0.95 \
--- jobs/classify_tfidf_fewshot_local.sh
+++ jobs/classify_tfidf_fewshot_local.sh
@@ -5,7 +5,7 @@ python classify.py \
     --original_reviews="data/amazon_reviews/original_reviews.txt" \
     --distilled_reviews="data/amazon_reviews/few-shot/distilled_reviews.txt" \
     --out_file="data/amazon_reviews/few-shot/results_tfidf_local.txt" \
-    --num_reviews=20 \
+    --num_reviews=5000 \
     --max_features=256 \
     --test_size=0.2 \
     --num_bootstrap=20 \
